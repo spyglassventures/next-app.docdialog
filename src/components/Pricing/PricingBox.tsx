07fba029f2aac7a46d7e6691e4b467a140848799
Daniel Mueller <dm@spyglassventures.ch>
2024-10-15 16:12:21 +0200

    calendly intgration

--- src/components/Pricing/PricingBox.tsx
+++ src/components/Pricing/PricingBox.tsx
@@ -25,9 +25,9 @@ const PricingBox = (props: {
         </div>
         <p className="mb-7 text-base text-body-color">{subtitle}</p>
         <div className="mb-8 border-b border-body-color border-opacity-10 pb-8 dark:border-white dark:border-opacity-10">
-          <Link href="/downloadsBestellungen">
+          <Link href="https://calendly.com/doc-dialog/30min">
             <button className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
-              Kostenlose Demo
+              Demo buchen
             </button>
           </Link>
         </div>
