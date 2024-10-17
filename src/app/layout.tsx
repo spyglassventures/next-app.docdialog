0bbae182edc1e6a7d09e2ae3f5ae7d03e9f1c79f
Dr. Daniel Müller <dm@spyglassventures.ch>
2024-10-17 21:30:52 +0200

    Update layout.tsx
    Switch GA ids 
    but not yet, 
    src="https://www.googletagmanager.com/gtag/js?id=G-YP0C4PGBG5"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','GTM-5QZFZCC7');`,
+                      })(window,document,'script','dataLayer','GTM-WSW5M6P8');`,
           }}
         />
       </head>
@@ -69,7 +69,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5QZFZCC7"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WSW5M6P8"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
